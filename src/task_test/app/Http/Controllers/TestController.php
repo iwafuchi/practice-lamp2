b8c4ec739d9541c9edffeb77479f91b2c0e56522
iwafuchi <rta375@gmail.com>
2022-07-23 22:00:23 +0900

    feat:Add whereRaw query

--- src/task_test/app/Http/Controllers/TestController.php
+++ src/task_test/app/Http/Controllers/TestController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+
 
 class TestController extends Controller {
     //
     public function index() {
         $values = Test::all();
-        // dd($values);
+
+        $tests = DB::table('tests')
+            ->select('id')
+            ->get();
+
+        $tests2 = DB::table('tests')
+            ->whereRaw('`id` = ? OR `text` = ?', ['1', 'bbb'])
+            ->get();
+        dd($tests2);
+        $collection = collect([1, 2, 3, 4, 5, 6, 7]);
+        $chunks = $collection->chunk(4);
+        $chunks->all();
+
+        dd($values, $tests, $chunks);
         return view('tests.test', compact('values'));
     }
 }
